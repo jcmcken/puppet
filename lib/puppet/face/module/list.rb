b7ed8d2e27c35457b8f9630107dc33f553410b6a
Matt Robinson <matt@puppetlabs.com>
2011-12-21 14:28:16 -0800

    wip: proof of concept for giving better module feedback
    This provides `puppet module list` as an action that will tell you what
    modules are in your modulepath, and what classes, defined types and
    hosts are defined in each module.
    
    This work still needs:
    * tests
    * ability to specify environment
    * cleaner code
    * integrated so that this info prints when a class or defined type can't
      be found - maybe only when --debug is on or something
      (lib/puppet/parser/compiler.rb:161)

---
+++ lib/puppet/face/module/list.rb
@@ -0,0 +1,36 @@
+require 'puppet/parser/type_loader'
+
+Puppet::Face.define(:module, '1.0.0') do
+  action(:list) do
+    summary "List modules"
+    description <<-EOT
+      List modules and hopefully the classes they provide
+    EOT
+
+    returns "list of modules and classes they provide"
+
+    examples <<-EOT
+      something
+    EOT
+
+    when_invoked do |*args|
+      environment = Puppet::Node::Environment.new('production')
+      type_loader = Puppet::Parser::TypeLoader.new(environment)
+      type_loader.import_all
+
+      module_contents = environment.known_resource_types.grouped_by_module
+
+      module_contents.each do |module_name, resource_types|
+        puts "Module #{module_name} contains:\n"
+        resource_types.each do |resource_type, names|
+          puts "  #{resource_type}"
+          names.each do |name|
+            puts "    #{name}"
+          end
+        end
+      end
+      ''
+    end
+
+  end
+end
